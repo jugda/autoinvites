5e9da83f21fdced6230f8d038e3eb13c8e5d816e
Niko Köbler <niko@n-k.de>
2015-12-15 18:33:59 +0100

    order matters

--- config.js
+++ config.js
@@ -1,5 +1,13 @@
 var argv = require('minimist')(process.argv.slice(2));
 
+var parseBoolean = function(arg, defaultValue) {
+  if (arg) {
+    regexp = /^(?:yes|y|true|t|on|1|ok)$/i;
+    return typeof arg === 'string' && arg.search(regexp) != -1;
+  }
+  return defaultValue;
+}
+
 config = {
   now: parseBoolean(argv.now, false)
 };
@@ -25,12 +33,3 @@ config.ical_url = argv.ical_url || 'http://www.jug-da.de/events.ics';
 if (argv.print_config) console.log(config);
 
 module.exports = config;
-
-
-var parseBoolean = function(arg, defaultValue) {
-  if (arg) {
-    regexp = /^(?:yes|y|true|t|on|1|ok)$/i;
-    return typeof arg === 'string' && arg.search(regexp) != -1;
-  }
-  return defaultValue;
-}
