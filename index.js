a813b6ab1b9b5f1328febf95af997f5f20b0f32c
Niko Köbler <niko@n-k.de>
2015-12-17 21:15:11 +0100

    separate sending mails into own module be able to execute actions on more days before an event

--- index.js
+++ index.js
@@ -1,28 +1,14 @@
 var ical = require('ical');
 var moment = require('moment');
-var nodemailer = require('nodemailer');
-var strip = require('strip');
 var config = require('./config');
-
-var buildMailText = function(ev) {
-  var date = moment(ev.start).format('dddd, DD. MMMM YYYY');
-  var time = moment(ev.start).format('HH:mm');
-  return '<p>Am kommenden <b>' + date + '</b> findet ab <b>' + time + ' Uhr</b> unser nächster Talk statt:</p>'
-      + '<p><h3>&nbsp;&nbsp;&nbsp;&nbsp;' + ev.summary + '</h3></p>'
-      + '<p>Ort: ' + ev.location + '</p>'
-      + '<p>Alle Infos zum Talk, Anfahrtsbeschreibung und Anmeldung wie immer auf unserer Website:<br/>'
-      + '<a href="' + ev.url + '">' + ev.url + '</a></p>'
-      + '<p>Wir freuen uns auf Euer zahlreiches Kommen!<br/><br/>Viele Grüße,<br/>Euer JUG DA Orga-Team</p>';
-}
-
+var mail = require('./mail');
 
 exports.handler = function(event, context) {
   var transporter = nodemailer.createTransport(config.smtp);
   var mailOptions = config.mailOptions;
 
   moment.locale('de');
-  var oneWeek = moment().add(7, 'days').startOf('day');
-  var twoDays = moment().add(2, 'days').startOf('day');
+  var today = moment().startOf('day');
 
   ical.fromURL(config.ical_url, {}, function(err, data) {
     if (err) {
@@ -33,16 +19,38 @@ exports.handler = function(event, context) {
           var ev = data[k];
           if (ev.start) {
             var m = moment(ev.start).startOf('day');
-            if (m.isSame(oneWeek) || m.isSame(twoDays) || config.now) {
-              mailOptions.html = buildMailText(ev);
-              mailOptions.text = strip(mailOptions.html);
-              mailOptions.subject = moment(ev.start).format('DD.MM.YYYY') + ': ' + ev.summary;
-              transporter.sendMail(mailOptions, function(error, info) {
-                  if (error) {
-                      return console.log(error);
-                  }
-                  console.log('Message sent: ' + info.response);
-              });
+            var diff = m.diff(today, 'days');
+            
+            switch (diff) {
+              case 0:
+
+                break;
+              case 1:
+
+                break;
+              case 2:
+                mail(ev);
+                break;
+              case 3:
+
+                break;
+              case 4:
+
+                break;
+              case 5:
+
+                break;
+              case 6:
+
+                break;
+              case 7:
+                mail(ev);
+                break;
+              default:
+                if (config.now) {
+                  mail(ev);
+                }
+                break;
             }
           }
         }
