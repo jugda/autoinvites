fc374ae4224371276871d912aa7310f916b1b8d1
Niko Köbler <niko@n-k.de>
2018-08-24 16:31:13 +0200

    drop smtp in favor for aws ses for sending emails

--- mail.js
+++ mail.js
@@ -1,8 +1,11 @@
 const moment = require('moment');
-const nodemailer = require('nodemailer');
+const AWS = require('aws-sdk');
 const strip = require('strip');
 
-const days = [2, 7];
+AWS.config.update({region: 'eu-west-1'});
+const ses = new AWS.SES({apiVersion: '2010-12-01'});
+
+const days = [2, 7, 28];
 
 const buildMailText = (ev) => {
   const date = moment(ev.start).format('dddd, DD. MMMM YYYY');
@@ -16,34 +19,56 @@ const buildMailText = (ev) => {
       + '<p><small><i>Eine Veranstaltung des iJUG e.V., organisiert durch die JUG Darmstadt.</i></small></p>';
 };
 
+const buildAnnouncingMailText = (ev) => {
+  const date = moment(ev.start).format('dddd, DD. MMMM YYYY [ab] HH:mm');
+  return 'Liebe JUG DA Freunde,'
+    + '<p>wir möchten Euch heute folgende Veranstaltung ankündigen:</p>'
+    + '<p><h3>&nbsp;&nbsp;&nbsp;&nbsp;' + ev.summary + '</h3></p>'
+    + '<p>Wo: ' + date + '<br/>'
+    + 'Wann: ' + ev.location + '</p>'
+    + '<p><em style="color: red">Die Anmeldung ist jetzt ebenfalls geöffnet!</em></p>'
+    + '<p>Alle weiteren Infos zum Talk, wie Anfahrtsbeschreibung und Anmeldung, gibt\'s wie immer auf unserer Website:<br/>'
+    + '<a href="' + ev.url + '">' + ev.url + '</a></p>'
+    + '<p>Wir freuen uns auf Euer zahlreiches Kommen!<br/><br/>Viele Grüße,<br/>Euer JUG DA Orga-Team</p>'
+    + '<p><small><i>Eine Veranstaltung des iJUG e.V., organisiert durch die JUG Darmstadt.</i></small></p>';
+};
+
+const getUtf8Data = data => {
+  return {
+    Charset: 'UTF-8',
+    Data: data
+  }
+};
+
 const sendMail = (ev, day) => {
   if (days.indexOf(day) > -1) {
     console.log('prepare mail to send for event uid ' + ev.uid);
-    const transporter = nodemailer.createTransport({
-      host: process.env.SMTP_HOST,
-      port: 465,
-      secure: true,
-      debug: false,
-      auth: {
-          user: process.env.SMTP_USER,
-          pass: process.env.SMTP_PASSWORD
-      }
-    });
-    const mailOptions = {
-      from: 'JUG DA <info@jug-da.de>',
-      to: 'jug-da@googlegroups.com'
+
+    const to = process.env.MAIL_TO;
+    const from = process.env.MAIL_FROM;
+    const htmlBody = day === 28 ? buildAnnouncingMailText(ev) : buildMailText(ev);
+    const textBody = strip(htmlBody);
+    const subject = (day === 28 ? 'Ankündigung für ' : '') + moment(ev.start).format('DD.MM.YYYY') + ': ' + ev.summary;
+
+    const email = {
+      Destination: { ToAddresses: [to] },
+      Message: {
+        Body: {
+          Html: getUtf8Data(htmlBody),
+          Text: getUtf8Data(textBody)
+        },
+        Subject: getUtf8Data(subject)
+      },
+      Source: from
     };
 
-    mailOptions.html = buildMailText(ev);
-    mailOptions.text = strip(mailOptions.html);
-    mailOptions.subject = moment(ev.start).format('DD.MM.YYYY') + ': ' + ev.summary;
-
-    transporter.sendMail(mailOptions, (error, info) => {
-      if (error) {
+    ses.sendEmail(email).promise()
+      .then(data => {
+        console.log('Message sent', data);
+      })
+      .catch(error => {
         throw error;
-      }
-      console.log('Message sent: ' + info.response);
-    });
+      });
   }
 };
 
