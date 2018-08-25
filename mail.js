b71f6a8df71cfa84932b66fe8373cce1a5aaf40c
Niko Köbler <niko@n-k.de>
2018-08-25 11:59:19 +0200

    use handlebars for mail templates

--- mail.js
+++ mail.js
@@ -1,37 +1,13 @@
-const moment = require('moment');
+const fs = require('fs');
 const AWS = require('aws-sdk');
+const moment = require('moment');
+const handlebars = require('handlebars');
 const strip = require('strip');
 
 AWS.config.update({region: 'eu-west-1'});
 const ses = new AWS.SES({apiVersion: '2010-12-01'});
 
 const days = [2, 7, 28];
-
-const buildMailText = (ev) => {
-  const date = moment(ev.start).format('dddd, DD. MMMM YYYY');
-  const time = moment(ev.start).format('HH:mm');
-  return '<p>Am kommenden <b>' + date + '</b> findet ab <b>' + time + ' Uhr</b> unser nächster Talk statt:</p>'
-      + '<p><h3>&nbsp;&nbsp;&nbsp;&nbsp;' + ev.summary + '</h3></p>'
-      + '<p>Ort: ' + ev.location + '</p>'
-      + '<p>Alle Infos zum Talk, Anfahrtsbeschreibung und Anmeldung wie immer auf unserer Website:<br/>'
-      + '<a href="' + ev.url + '">' + ev.url + '</a></p>'
-      + '<p>Wir freuen uns auf Euer zahlreiches Kommen!<br/><br/>Viele Grüße,<br/>Euer JUG DA Orga-Team</p>'
-      + '<p><small><i>Eine Veranstaltung des iJUG e.V., organisiert durch die JUG Darmstadt.</i></small></p>';
-};
-
-const buildAnnouncingMailText = (ev) => {
-  const date = moment(ev.start).format('dddd, DD. MMMM YYYY [ab] HH:mm');
-  return 'Liebe JUG DA Freunde,'
-    + '<p>wir möchten Euch heute folgende Veranstaltung ankündigen:</p>'
-    + '<p><h3>&nbsp;&nbsp;&nbsp;&nbsp;' + ev.summary + '</h3></p>'
-    + '<p>Wo: ' + date + '<br/>'
-    + 'Wann: ' + ev.location + '</p>'
-    + '<p><em style="color: red">Die Anmeldung ist jetzt ebenfalls geöffnet!</em></p>'
-    + '<p>Alle weiteren Infos zum Talk, wie Anfahrtsbeschreibung und Anmeldung, gibt\'s wie immer auf unserer Website:<br/>'
-    + '<a href="' + ev.url + '">' + ev.url + '</a></p>'
-    + '<p>Wir freuen uns auf Euer zahlreiches Kommen!<br/><br/>Viele Grüße,<br/>Euer JUG DA Orga-Team</p>'
-    + '<p><small><i>Eine Veranstaltung des iJUG e.V., organisiert durch die JUG Darmstadt.</i></small></p>';
-};
 
 const getUtf8Data = data => {
   return {
@@ -46,9 +22,16 @@ const sendMail = (ev, day) => {
 
     const to = process.env.MAIL_TO;
     const from = process.env.MAIL_FROM;
-    const htmlBody = day === 28 ? buildAnnouncingMailText(ev) : buildMailText(ev);
-    const textBody = strip(htmlBody);
     const subject = (day === 28 ? 'Ankündigung für ' : '') + moment(ev.start).format('DD.MM.YYYY') + ': ' + ev.summary;
+
+    ev.date = moment(ev.start).format('dddd, DD. MMMM YYYY');
+    ev.time = moment(ev.start).format('HH:mm');
+
+    const templateName = day === 28 ? 'announcement' : 'invitation';
+    const source = fs.readFileSync(`templates/mail_${templateName}.hbs`, 'utf-8');
+    const template = handlebars.compile(source);
+    const htmlBody = template(ev);
+    const textBody = strip(htmlBody);
 
     const email = {
       Destination: { ToAddresses: [to] },
