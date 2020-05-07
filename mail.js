bd9cd6b220d949a209077786e771e6e10717c3e8
Niko Köbler <niko@n-k.de>
2020-05-07 18:49:32 +0200

    send pre-announcement not when there's no registration planned

--- mail.js
+++ mail.js
@@ -18,40 +18,42 @@ const getUtf8Data = data => {
 
 const sendMail = (ev, day) => {
   if (days.indexOf(day) > -1) {
-    console.log('prepare mail to send for event uid ' + ev.uid);
+    if (day !== 28 || !ev.hideRegistration) {
+      console.log('prepare mail to send for event uid ' + ev.uid);
 
-    const to = process.env.MAIL_TO;
-    const from = process.env.MAIL_FROM;
-    const subject = (day === 28 ? 'Ankündigung für ' : '') + moment(ev.start).format('DD.MM.YYYY') + ': ' + ev.summary;
+      const to = process.env.MAIL_TO;
+      const from = process.env.MAIL_FROM;
+      const subject = (day === 28 ? 'Ankündigung für ' : '') + moment(ev.start).format('DD.MM.YYYY') + ': ' + ev.summary;
 
-    ev.date = moment(ev.start).format('dddd, DD. MMMM YYYY');
-    ev.time = moment(ev.start).format('HH:mm');
+      ev.date = moment(ev.start).format('dddd, DD. MMMM YYYY');
+      ev.time = moment(ev.start).format('HH:mm');
 
-    const templateName = day === 28 ? 'announcement' : 'invitation';
-    const source = fs.readFileSync(`templates/mail_${templateName}.hbs`, 'utf-8');
-    const template = handlebars.compile(source);
-    const htmlBody = template(ev);
-    const textBody = strip(htmlBody);
+      const templateName = day === 28 ? 'announcement' : 'invitation';
+      const source = fs.readFileSync(`templates/mail_${templateName}.hbs`, 'utf-8');
+      const template = handlebars.compile(source);
+      const htmlBody = template(ev);
+      const textBody = strip(htmlBody);
 
-    const email = {
-      Destination: { ToAddresses: [to] },
-      Message: {
-        Body: {
-          Html: getUtf8Data(htmlBody),
-          Text: getUtf8Data(textBody)
+      const email = {
+        Destination: { ToAddresses: [to] },
+        Message: {
+          Body: {
+            Html: getUtf8Data(htmlBody),
+            Text: getUtf8Data(textBody)
+          },
+          Subject: getUtf8Data(subject)
         },
-        Subject: getUtf8Data(subject)
-      },
-      Source: from
-    };
+        Source: from
+      };
 
-    ses.sendEmail(email).promise()
-      .then(data => {
-        console.log('Message sent', data);
-      })
-      .catch(error => {
-        throw error;
-      });
+      ses.sendEmail(email).promise()
+        .then(data => {
+          console.log('Message sent', data);
+        })
+        .catch(error => {
+          throw error;
+        });
+    }
   }
 };
 
